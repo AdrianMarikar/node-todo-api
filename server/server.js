2f403cb54478f0c3acf63a1e8828eed99cfcbcd1
Adrian Marikar <adrianmarikar@gmail.com>
2017-02-15 22:41:58 +0000

    Associate users with todos.

--- server/server.js
+++ server/server.js
@@ -15,9 +15,10 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
 	var todo = new Todo({
-		text: req.body.text
+		text: req.body.text,
+		_creator: req.user._id
 	});
 
 	todo.save().then((doc) => {
@@ -28,8 +29,10 @@ app.post('/todos', (req, res) => {
 });
 
 
-app.get('/todos', (req, res) => {
-	Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+	Todo.find({
+		_creator: req.user._id
+	}).then((todos) => {
 		res.send({todos});
 	}).catch((e) => {
 		res.status(400).send(e);
@@ -47,7 +50,7 @@ app.get('/todos', (req, res) => {
 		//error
 			//400 - and send empty body back
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
 	var id = req.params.id;
 //validate id using isValid
 
@@ -55,7 +58,10 @@ if (!ObjectID.isValid(id)) {
 	return res.status(404).send();
 }
 
-	Todo.findById(id).then((todo) => {
+	Todo.findOne({
+		_id: id,
+		_creator: req.user._id
+	}).then((todo) => {
 		if (!todo) {
 			return res.status(404).send();
 		}
@@ -65,7 +71,7 @@ if (!ObjectID.isValid(id)) {
 	});
 });
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
 	// get the id
 	var id = req.params.id;
 
@@ -77,7 +83,10 @@ app.delete('/todos/:id', (req, res) => {
 		//success
 			//if no doc, send 404
 			//if doc, send doc back with a 200
-	Todo.findByIdAndRemove(id).then((todo) => {
+	Todo.findOneAndRemove({
+		_id: id,
+		_creator: req.user._id
+	}).then((todo) => {
 	if (!todo) {
 			return res.status(404).send();
 		}
@@ -89,7 +98,7 @@ app.delete('/todos/:id', (req, res) => {
 	});
 });
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
 	var id = req.params.id;
 	//Choose what can be updated
 	var body = _.pick(req.body, ['text', 'completed']);
@@ -105,7 +114,13 @@ app.patch('/todos/:id', (req, res) => {
 		body.completedAt = null;
 	}
 
-	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+	Todo.findOneAndUpdate({
+		_id: id,
+		_creator: req.user._id
+	},
+		{$set: body},
+		{new: true}
+	).then((todo) => {
 		if(!todo) {
 			return res.status(404).send();
 		}
