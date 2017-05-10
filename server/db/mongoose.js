8dbb8626c6f7da40290335b5e1f7fc45f7513db4
Adrian Marikar <adrianmarikar@gmail.com>
2017-05-10 21:34:49 +0100

    Gave mongoose.connect an OR operator for localhost

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
 
